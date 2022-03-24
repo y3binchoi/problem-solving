afb596088665d048a4217bd9d90ffa1f77613164
devin <dev.devin.kr@gmail.com>
2022-03-24 21:22:40 +0900

    #1 FIX sportwear.py

--- Programmers/sportwear.py
+++ Programmers/sportwear.py
@@ -1,16 +1,16 @@
 def solution(n, lost, reserve):
     answer = n  # 전체 학생 수에서 체육복을 못 빌린 학생 수 빼기
-    lost.sort()
-    reserve.sort()
-    for l in lost:
-        if l in reserve:  # 본인의 여벌 체육복
-            reserve = [i for i in reserve if i != l]
+    ex_lost = [i for i in lost if i not in reserve]  # 여벌 있는 사람은 자기꺼 입어라
+    ex_reserve = [i for i in reserve if i not in lost]
+    ex_lost.sort()
+    ex_reserve.sort()
+
+    for l in ex_lost:
+        if l - 1 in ex_reserve:  # 앞 번호의 여벌 체육복
+            ex_reserve = [i for i in ex_reserve if i != l - 1]
             continue
-        elif l - 1 in reserve:  # 앞 번호의 여벌 체육복
-            reserve = [i for i in reserve if i != l - 1]
-            continue
-        elif l + 1 in reserve:  # 뒷 번호의 여벌 체육복
-            reserve = [i for i in reserve if i != l + 1]
+        elif l + 1 in ex_reserve:  # 뒷 번호의 여벌 체육복
+            ex_reserve = [i for i in ex_reserve if i != l + 1]
             continue
         answer -= 1
     return answer
